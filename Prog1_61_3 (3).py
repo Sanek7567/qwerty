becd9c035fcabc0f06b291e9959a2ca5921029fc
Sanek7567 <123890124+Sanek7567@users.noreply.github.com>
2023-02-05 15:43:24 +0300

    Add files via upload

---
+++ Prog1_61_3 (3).py
@@ -0,0 +1,53 @@
+
+############################################################################# знакомство с tkinter
+
+#from tkinter import *
+#root = Tk()
+#root.title('window')
+#root.geometry('500x300')
+#def function1():
+#    w1.config(text=int(w2.get())*2)
+#w2=Entry(root, bg = 'green', fg = 'white')
+#w3=Button(root, text='кнопка', width = 30, height = 20, command=function1, activebackground = 'yellow', bg = 'purple', fg = 'cyan')
+#w1=Label(root, text='Это подпись', width=15, bg = 'cyan', font = 'consolas')
+#w1.pack()
+#w2.pack()
+#w3.pack()
+#root.mainloop()
+
+############################################################################# фигуры
+
+#from tkinter import *
+#root = Tk()
+#root.title('window')
+#root.geometry('1000x1300')
+#def function1():
+#    canvas.itemconfig(b, fill = 'purple')
+#def function2():
+#    canvas.itemconfig(b, fill = 'red')
+#canvas = Canvas(root, width=800,height=800, bg = 'green')
+#canvas.pack()
+#canvas.create_line(15,15,150,150, width = 5)
+#canvas.create_rectangle(110,130,180,180, width = 5, fill = 'yellow')
+#b = canvas.create_oval(280,230,720,780, fill = 'red', outline = 'white', width = 8, activefill = 'white', activeoutline = 'pink')
+#a = canvas.create_polygon(450,280,310,480,240,290,335,340, width = 5, fill = 'brown', outline = 'cyan', dash=10)
+#Button(root, text='кнопка1', width = 30, height = 5, command=function1, activebackground = 'yellow', bg = 'purple', fg = 'cyan').pack()
+#Button(root, text='кнопка2', width = 30, height = 5, command=function2, activebackground = 'yellow', bg = 'purple', fg = 'cyan').pack()
+#root.mainloop()
+
+############################################################################# messagebox
+
+#from tkinter import *
+#from tkinter import messagebox
+#root = Tk()
+#root.title('window')#подпись окна
+#root.geometry('300x300')#геометрия
+#def function1():
+#    messagebox.showinfo('Вы получили', int(a.get())*15)
+#a = Entry(root, width = 10, bg = 'gray', fg = 'white')
+#a.pack()
+#Button(root, text='умножить на 15', width = 30, height = 5, command=function1, bg = 'cyan', fg = 'black').pack()
+#root.mainloop()
+
+#############################################################################
+
